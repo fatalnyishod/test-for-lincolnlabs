09590ad5aa685901ae03c130bb2cf886d4316117
fatalnyishod <fatalnyishod@gmail.com>
2020-07-25 20:44:13 +0300

    complete commit

---
+++ main.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  nav:false,
+  dots:true,
+  margin:20,
+  responsive:{
+      0:{
+          items:1
+      },
+      769:{
+          items:2
+      }
+  }
+});
